e39e012de225a48cc9dba6d415f8cb84b016f906
meredithheller <meredithheller@outlook.com>
2018-08-15 14:11:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name'(name) do
+    return "#{name}".reverse
+  end
+  get '/square/:number' do
+    #{number}.to_i ^2
 
 end
